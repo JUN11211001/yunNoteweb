8d541993f632d92f389a06ca15e2af6e191b0a6c
yun <2293687468@qq.com>
2025-05-16 08:35:51 +0800

    perf:
    解决页面空白问题，将路由模式改为Hash模式

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
 import Home from '../pages/Home.vue'
 import Learning from '../pages/Learning.vue'
 import Tools from '../pages/Tools.vue'
@@ -7,7 +7,7 @@ import Skills from '../pages/Skills.vue'
 import Knowledge from '../pages/Knowledge.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
